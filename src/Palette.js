4c3306f4c8b80d9cd45e4553c6a0ec6dae167ccb
partyVic <vicwang128@gmail.com>
2022-01-14 14:07:13 +1100

    Style Color Slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,30 +1,47 @@
+import { useState } from "react";
 import ColorBox from "./ColorBox"
-import "./Palette.css"
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
-import { useState } from "react";
+import "./Palette.css"
 
 function Palette(props) {
     const [level, setLevel] = useState(500)
-    const {colors} = props.palette
+    const { colors } = props.palette
 
     const colorBoxes = colors[level].map(color => (
         <ColorBox background={color.hex} name={color.name} />
     ))
 
-    const changeLevel = (newLevel) =>{
+    const changeLevel = (newLevel) => {
         setLevel(newLevel)
     }
 
     return (
         <div className="Palette">
-            <Slider 
-                defaultValue={level}
-                min={100}
-                max={900}
-                step={100}
-                onAfterChange={changeLevel}
-            />
+            <div className="slider">
+                <Slider
+                    defaultValue={level}
+                    min={100}
+                    max={900}
+                    step={100}
+                    onAfterChange={changeLevel}
+
+                    //in-line style recommended by re-slider
+                    trackStyle={[{ backgroundColor: "transparent" }]}
+                    handleStyle={[
+                        {
+                            backgroundColor: "green",
+                            outline: "none",
+                            border: "2px solid green",
+                            boxShadow: "none",
+                            width: "13px",
+                            height: "13px",
+                            marginLeft: "-7px",
+                        },
+                    ]}
+                    railStyle={{ height: "8" }}
+                />
+            </div>
             <div className="Palette-colors">
                 {colorBoxes}
             </div>
