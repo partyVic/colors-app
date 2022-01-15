6c35b526088f92e4138961e1473fd7627abd21bd
partyVic <vicwang128@gmail.com>
2022-01-15 21:55:42 +1100

    Add palette footer

--- src/Palette.js
+++ src/Palette.js
@@ -6,10 +6,10 @@ import "./Palette.css"
 function Palette(props) {
     const [level, setLevel] = useState(500)
     const [format, setFormat] = useState("hex")
-    const { colors } = props.palette
+    const { colors, paletteName, emoji } = props.palette
 
     const colorBoxes = colors[level].map(color => (
-        <ColorBox background={color[format]} name={color.name} />
+        <ColorBox background={color[format]} name={color.name} key={color.id}/>
     ))
 
     const changeLevel = (newLevel) => {
@@ -31,6 +31,10 @@ function Palette(props) {
             <div className="Palette-colors">
                 {colorBoxes}
             </div>
+            <footer className="Palette-footer">
+                {paletteName}
+                <span className="emoji">{emoji}</span>
+            </footer>
         </div>
     )
 }
