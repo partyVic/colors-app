2db9b05fd9d3baeb64f8cf2f3ad4fa544b9a4935
partyVic <vicwang128@gmail.com>
2022-01-19 22:27:23 +1100

    Start styling PaletteList with MUI System & fixed some bugs on last commit

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,41 +1,43 @@
 import { Box } from "@mui/system"
 
-const root = {
-    bgColor: "white",
-    border: "1px solid black",
-    borderRadius: "5px",
-    padding: "0.5rem",
-    position: "relative",
-    overflow: "hidden",
-    "&:hover": {
-        cursor: "pointer"
+const styles = {
+    root: {
+        backgroundColor: "white",
+        border: "1px solid black",
+        borderRadius: "5px",
+        padding: "0.5rem",
+        position: "relative",
+        overflow: "hidden",
+        "&:hover": {
+            cursor: "pointer"
+        }
+    },
+    colors: {
+        backgroundColor: "grey",
+    },
+    title: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        margin: "0",
+        color: "black",
+        paddingTop: "0.5rem",
+        fontSize: "1rem",
+        position: "relative"
+    },
+    emoji: {
+        marginLeft: "0.5rem",
+        fontSize: "1.5rem"
     }
-}
-const colors = {
-    bgColor: "grey",
-}
-const title = {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    margin: "0",
-    color: "black",
-    paddingTop: "0.5rem",
-    fontSize: "1rem",
-    position: "relative"
-}
-const spanEmoji = {
-    marginLeft: "0.5rem",
-    fontSize: "1.5rem"
 }
 
 function MiniPalette(props) {
     const { paletteName, emoji } = props
     return (
-        <Box sx={root}>
-            <Box sx={colors} />
-            <Box sx={title} component="h5">
-                {paletteName} <Box component="span" style={{ spanEmoji }}>{emoji}</Box>
+        <Box sx={styles.root}>
+            <Box sx={styles.colors} />
+            <Box sx={styles.title} component="h5">
+                {paletteName} <Box component="span" sx={styles.emoji}>{emoji}</Box>
             </Box>
         </Box>
     )
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,16 +1,52 @@
 import MiniPalette from "./MiniPalette"
 import { Link } from "react-router-dom"
+import { Box } from "@mui/system"
+
+const styles = {
+    root: {
+        backgroundColor: "blue",
+        height: "100%",
+        display: "flex",
+        alignItems: "flex-start",
+        justifyContent: "center"
+    },
+    container: {
+        width: "50%",
+        display: "flex",
+        alignItems: "flex-start",
+        flexDirection: "column",
+        flexWrap: "wrap",
+    },
+    nav: {
+        color: "white",
+        display: "flex",
+        width: "100%",
+        justifyContent: "space-between"
+    },
+    palettes: {
+        boxSizing: "border-box",
+        width: "100%",
+        display: "grid",
+        gridTemplateColumns: "repeat(3, 30%)",
+        gap: "5%"
+    }
+}
 
 function PaletteList(props) {
     const { palettes } = props
     return (
-        <div>
-            <MiniPalette />
-            <h1>React colors</h1>
-            {palettes.map(palette => (
-                <MiniPalette {...palette} key={palette.id} />
-            ))}
-        </div>
+        <Box sx={styles.root}>
+            <Box sx={styles.container}>
+                <Box component="nav" sx={styles.nav}>
+                    <h1>React Colors</h1>
+                </Box>
+                <Box sx={styles.palettes}>
+                    {palettes.map(palette => (
+                        <MiniPalette {...palette} key={palette.id} />
+                    ))}
+                </Box>
+            </Box>
+        </Box>
     )
 }
 
