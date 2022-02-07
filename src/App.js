d9b0393158feb1239135037f14e84cc0bbba0aab
partyVic <vicwang128@gmail.com>
2022-02-07 18:38:10 +1100

    Save palettes to local storage in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router";
 import Palette from "./Palette";
 import PaletteList from "./PaletteList";
@@ -7,18 +7,29 @@ import NewPaletteForm from "./NewPaletteForm";
 import seedColors from "./seedColors";
 
 function App() {
-  const [palettes, setPalettes] = useState(seedColors)
+  const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"))
+  const [palettes, setPalettes] = useState(savedPalettes || seedColors)
 
   const savePalette = (newPalette) => {
     setPalettes([...palettes, newPalette])
   }
- 
+
+  const syncLocalStorage = () => {
+    //save palettes to local storage
+    window.localStorage.setItem(
+      "palettes",
+      JSON.stringify(palettes)
+    )
+  }
+
+  useEffect(syncLocalStorage, [palettes])
+
   return (
     <Routes>
-      <Route path="/palette/new" element={<NewPaletteForm savePalette={savePalette} palettes={palettes}/>} />
-      <Route path="/palette/:paletteId/:colorId" element={<SingleColorPalette palettes={palettes}/>} />
+      <Route path="/palette/new" element={<NewPaletteForm savePalette={savePalette} palettes={palettes} />} />
+      <Route path="/palette/:paletteId/:colorId" element={<SingleColorPalette palettes={palettes} />} />
       <Route path="/" element={<PaletteList palettes={palettes} />} />
-      <Route path="/palette/:id" element={<Palette palettes={palettes}/>} />
+      <Route path="/palette/:id" element={<Palette palettes={palettes} />} />
     </Routes>
   );
 }
